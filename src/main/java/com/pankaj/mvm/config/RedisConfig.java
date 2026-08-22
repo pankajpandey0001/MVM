f0cf2131b96f18d7e34ea34b2ccbac9b1434d10c
pankajpandey0001 <i.pankajpandey0001@gmail.com>
2026-08-22 13:43:48 +0530

    Update RedisConfig.java

--- src/main/java/com/pankaj/mvm/config/RedisConfig.java
+++ src/main/java/com/pankaj/mvm/config/RedisConfig.java
@@ -1,3 +1,5 @@
+package com.pankaj.mvm.config;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Value;
